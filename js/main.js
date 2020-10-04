ca0c9813bfcbb5ffcb678280e1531b0c640f4da1
Igor Cruz <igorcruz.it@outlook.com>
2020-10-04 16:32:10 -0300

    atualização de aula

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+
+//function botão(){
+  //  document.getElementById("agradecimento").innerHTML = "<b> obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+//}
+
+/*function soma (n1,n2){
+    return n1 + n2;
+}
+function validaIdade (idade){
+    var validar;
+    if(idade >= 18){
+        validar = true
+    } else{
+        validar = false
+    }
+    return validar;
+}
+var idade = prompt("Qual sua idade ? ");
+console.log(validaIdade(idade));*/
+
+
+
+
+/*var d = new Date();
+alert(d.getUTCDay());
+alert(d.getMonth());
+alert(d.getMinutes());*/
+
+/*var count;
+for (count=0; count <=5; count++){
+    alert(count);
+};*/
+
+/*var count = 5;
+while(count <= 5 ){
+    console.log(count);
+    alert(count)
+    count ++;
+}*/
+
+/*var idade = prompt("Qual sua idade ?");
+if(idade >= 18){
+    alert ("maior de idade");
+}else{
+    alert("menor de idade");
+};*/
+
+/*var fruta = {nome:"maçã", cor:"Vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor); */
+
+//var lista = ["maçã", "morango", "banana"]
+//lista.push("uva") //adiciona item a lista
+//lista.pop(); // retira último item da lista
+//console.log(lista[1]);
+//alert(lista);
+
+//var nome='Igor Cruz';
+//var idade = 26
+//var idade2 = 10; 
+//var frase = 'Japão é o melhor time do mund'
+//alert( nome + ' tem ' + idade + " anos ");
+//alert (idade + idade2   );
+//console.log (nome);
+//console.log(idade + idade2);
+//console.log(frase);
+//alert(frase.replace("Japão","Brasil")); // Substitui 
